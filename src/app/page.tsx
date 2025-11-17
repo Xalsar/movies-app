6119c967b1e0416de3782c050867639fcf1b93c3
Xavier Carrera Gimbert <xavicarreragimbert@gmail.com>
2025-11-17 14:06:36 +0100

    feat: remove unused imports and search movies hook from HomePage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,9 @@
 "use client";
 
-import { Input } from "~/components/ui/input";
-
-import { MovieCard } from "./components/movie-card";
-import { LoadingSpinner } from "./components/loading-spinner";
-import { Alert, AlertDescription, AlertTitle } from "~/components/ui/alert";
-import { useSearchMovies } from "./containers/search-movies/hooks/use-search-movies";
 import { PopularMovies } from "./containers/popular-movies/popular-movies";
 import { SearchMovies } from "./containers/search-movies/search-movies";
 
 export default function HomePage() {
-  const {
-    query,
-    handleInputChange,
-    data: searchMoviesData,
-    error: searchMoviesError,
-    isLoading: searchMoviesIsLoading,
-    noMoviesFound,
-    didUserStartSearching,
-  } = useSearchMovies();
-
   return (
     <div className="mx-auto max-w-7xl py-3">
       <SearchMovies />
