874b30d69e66e10beff6e9e8a07ee006cf69954a
Xavier Carrera Gimbert <xavicarreragimbert@gmail.com>
2025-11-17 15:49:56 +0100

    refactor: remove console log for popularMoviesData

--- src/app/containers/popular-movies/popular-movies.tsx
+++ src/app/containers/popular-movies/popular-movies.tsx
@@ -10,8 +10,6 @@ export const PopularMovies = () => {
     isLoading: popularMoviesIsLoading,
   } = useFetchPopularMovies();
 
-  console.log("popularMoviesData", popularMoviesData);
-
   return (
     <div className="mt-10">
       <h2 className="mb-2 text-2xl font-bold">Popular movies</h2>
