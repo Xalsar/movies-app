d8e048adbf7b71b69a2de95470edd35465b8cc9f
Xavier Carrera Gimbert <xavicarreragimbert@gmail.com>
2025-11-17 10:49:58 +0100

    feat: use shadcdn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,26 @@
 import Image from "next/image";
+import { Button } from "~/components/ui/button";
+
+import { Card, CardContent, CardTitle } from "~/components/ui/card";
+import { Input } from "~/components/ui/input";
 
 const MovieCard = () => {
   return (
-    <div className="w-1xl my-3 flex gap-4 rounded border border-gray-300 p-5 shadow-md">
-      <Image
-        src="https://picsum.photos/200/300"
-        alt="Movie poster"
-        width={200}
-        height={300}
-      />
-      <div>
-        <p className="text-2xl font-bold text-blue-700">
-          The return of the Jedi
-        </p>
-        <p>01/1999</p>
-        <p>5/5</p>
-      </div>
-    </div>
+    <Card className="my-3 block w-xl">
+      <CardContent className="flex gap-3">
+        <Image
+          src="https://picsum.photos/200/300"
+          alt="Movie poster"
+          width={200}
+          height={300}
+        />
+        <div>
+          <CardTitle className="text-xl">The return of the Jedi</CardTitle>
+          <p>01/1999</p>
+          <p>5/5</p>
+        </div>
+      </CardContent>
+    </Card>
   );
 };
 
@@ -24,13 +28,8 @@ export default function HomePage() {
   return (
     <div className="mx-auto max-w-7xl py-3">
       <form className="flex gap-3">
-        <input
-          className="block flex-1 rounded border border-gray-300 p-3 shadow"
-          name="search"
-          placeholder="Search for a movie..."
-          id="search"
-        />
-        <button className="rounded border p-3">Search</button>
+        <Input name="search" placeholder="Search for a movie..." id="search" />
+        <Button className="rounded border p-3">Search</Button>
       </form>
       <div className="flex flex-wrap items-center justify-evenly">
         <MovieCard />
@@ -40,9 +39,7 @@ export default function HomePage() {
         <MovieCard />
       </div>
       <div className="mt-10">
-        <h2 className="mb-3 text-3xl font-bold text-blue-700">
-          Popular movies
-        </h2>
+        <h2 className="mb-3 text-3xl font-bold">Popular movies</h2>
         <div className="flex flex-wrap items-center justify-evenly">
           <MovieCard />
           <MovieCard />
