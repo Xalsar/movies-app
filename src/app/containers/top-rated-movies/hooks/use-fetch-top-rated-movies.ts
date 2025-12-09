cbbb2df2158444e64a15459f9e011045ddfb63b2
Xavier Carrera Gimbert <xavicarreragimbert@gmail.com>
2025-12-09 17:15:43 +0100

    feat: implement useFetchTopRatedMovies hook and update TopRatedMovies component

---
+++ src/app/containers/top-rated-movies/hooks/use-fetch-top-rated-movies.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+import useSWR from "swr";
+
+import type { Movie } from "~/types/movie";
+
+export function useFetchTopRatedMovies() {
+  const fetcher = (url: string) =>
+    axios(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+    }).then((res) => res.data);
+  const { data, error, isLoading } = useSWR<{
+    page: number;
+    results: Array<Movie>;
+    total_pages: number;
+    total_results: number;
+  }>(
+    `${process.env.NEXT_PUBLIC_API_URL}/movie/top_rated?language=en-US&page=1&api_key=${process.env.NEXT_PUBLIC_API_KEY}`,
+    fetcher,
+  );
+
+  return {
+    data,
+    error,
+    isLoading,
+  };
+}
--- src/app/containers/top-rated-movies/top-rated-movies.tsx
+++ src/app/containers/top-rated-movies/top-rated-movies.tsx
@@ -1,3 +1,44 @@
+import { LoadingSpinner } from "~/app/components/loading-spinner";
+import { useFetchTopRatedMovies } from "./hooks/use-fetch-top-rated-movies";
+import { MovieCard } from "~/app/components/movie-card";
+import { Alert, AlertDescription, AlertTitle } from "~/components/ui/alert";
+import { AlertCircleIcon } from "lucide-react";
+
 export const TopRatedMovies = () => {
-  return <div>Top Rated Movies Component</div>;
+  const { data, error, isLoading } = useFetchTopRatedMovies();
+
+  if (isLoading) {
+    return <LoadingSpinner>Loading top rated movies...</LoadingSpinner>;
+  }
+
+  if (error) {
+    return (
+      <Alert variant="destructive">
+        <AlertCircleIcon />
+        <AlertTitle>Error loading top rated movies</AlertTitle>
+        <AlertDescription>
+          An unexpected error occurred while fetching top rated movies. Please
+          try again later. Contact support if the problem persists.
+        </AlertDescription>
+      </Alert>
+    );
+  }
+
+  return (
+    <div className="flex flex-wrap items-center justify-evenly">
+      {data?.results.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          title={movie.title}
+          imageUrl={
+            movie.poster_path &&
+            `${process.env.NEXT_PUBLIC_API_IMAGE_URL}/w500${movie.poster_path}`
+          }
+          releaseDate={movie.release_date}
+          rating={`${movie.vote_average.toFixed(1)}/10`}
+          href={`/movie/${movie.id}`}
+        />
+      ))}
+    </div>
+  );
 };
